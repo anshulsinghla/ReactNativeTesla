6cdcb8aab980097ade932be0e42cc0cdbb2626fd
Anshul <love2anshul@gmail.com>
2021-02-11 17:54:04 +0530

    Simple tesla clone using react native

---
+++ App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, View } from "react-native";
+import Header from "./components/Header";
+import CarsList from "./components/CarList";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <CarsList />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
